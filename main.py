02cc46239080dae96085001db3068a99273d5086
Nikita <vekshinnikita>
2024-05-21 19:44:26 +0300

    fix: hide token

--- main.py
+++ main.py
@@ -2,15 +2,25 @@
 
 import asyncio
 import logging
+import os
 from aiogram import Bot, Dispatcher
 from app.handlers import router
+from dotenv import load_dotenv
 
-bot = Bot(token='6561890386:AAHWRJv1g1JjPsFASbAAyewTiuTHb4u4oIk')
+load_dotenv()
+
+token = os.getenv('TOKEN_TELEGRAM_BOT')
+
+if(token is None):
+    raise Exception('There is no telegram bot token')
+
+bot = Bot(token=token)
 dp = Dispatcher()
 
 async def main() -> None:
     dp.include_router(router)
     await dp.start_polling(bot)
+    
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
